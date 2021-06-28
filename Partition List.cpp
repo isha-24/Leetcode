d5e20e2faa9351c5ff2930a608f2a60e19a3a69d
isha-24 <55746937+isha-24@users.noreply.github.com>
2021-06-28 20:27:09 +0530

    Create Partition List.cpp

---
+++ Partition List.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* first=new ListNode();
+        ListNode* f=first;
+        ListNode* second=new ListNode();
+        ListNode* s=second;
+        ListNode* node;
+        
+        for(ListNode* h=head; h; h=node){
+            node=h->next;
+            if(h->val < x){
+                first->next=h;
+                first=first->next;
+            }
+            else{
+                second->next=h;
+                second=second->next;
+                second->next=NULL;
+            }
+        }
+        first->next=s->next;
+        return f->next;
+    }
+};

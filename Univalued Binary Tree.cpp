6315bf7635265c63ac7e01989ecd55ebddc14e82
isha-24 <55746937+isha-24@users.noreply.github.com>
2021-08-21 12:39:00 +0530

    Create Univalued Binary Tree.cpp

---
+++ Univalued Binary Tree.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/univalued-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        if(!root) return true;
+        bool l=root->left==NULL || (root->val == root->left->val && isUnivalTree(root->left));
+        bool r=root->right==NULL || (root->val == root->right->val && isUnivalTree(root->right));
+        return l && r;
+    }
+};

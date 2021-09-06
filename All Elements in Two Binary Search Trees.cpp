ec78a1cb6ed230ba773a56379f3ded96455818e9
isha-24 <55746937+isha-24@users.noreply.github.com>
2021-09-06 11:13:28 +0530

    Create All Elements in Two Binary Search Trees.cpp

---
+++ All Elements in Two Binary Search Trees.cpp
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/all-elements-in-two-binary-search-trees/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root, vector<int> &v){
+        if(!root) return;
+        helper(root->left, v);
+        v.push_back(root->val);
+        helper(root->right, v);
+    }
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> v;
+        helper(root1, v); helper(root2, v);
+        sort(v.begin(),v.end());
+        return v;
+    }
+};

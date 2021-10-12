cdae8d81a5480b72b1fec607b22964bd42afc911
isha-24 <55746937+isha-24@users.noreply.github.com>
2021-10-13 00:35:55 +0530

    Create Design HashSet.cpp

---
+++ Design HashSet.cpp
@@ -0,0 +1,32 @@
+//
+
+class MyHashSet {
+public:
+    /** Initialize your data structure here. */
+    vector<int> v;
+    MyHashSet() {
+        for(int i=0;i<=1000000;i++)
+            v.push_back(0);
+    }
+    
+    void add(int key) {
+        v[key]=1;
+    }
+    
+    void remove(int key) {
+        v[key]=0;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        return v[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */

0274c0b2d25cf8cf90c959edf0dab2ff33c815fc
isha-24 <55746937+isha-24@users.noreply.github.com>
2021-10-13 00:39:20 +0530

    Create Contains Duplicate.cpp

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i : nums){
+            if(s.find(i)==s.end())
+                s.insert(i);
+            else return true;
+        }
+        return false;
+    }
+};
